3623f9a72e94d06d7e19232a51e27988a54fef71
Gustavo <gustavooliveirabra@gmail.com>
2026-05-12 16:24:21 -0300

    Calculadora de Bhaskara

---
+++ projeto-calculadora-de-bhaskara/calculadora-baskara.py
@@ -0,0 +1,24 @@
+#projeto teste para calcular a formula de bhaskara
+#-----------------------------#
+
+a = float(input("Informe o valor de A"))
+b = float(input("Informe o valor de B"))
+c = float(input("Informe o valor de C"))
+#calculo do delta
+delta = b * b - 4 * a *c
+
+#verificaçao das condições com o elif
+
+if delta>0.0:
+    #calculo de 2 valores para x
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print("Para a equação {}x² + {}x + {} = 0, obtivemos os seguintes valores: x1 = {} e x2 = {}".format(a,b,c,x1,x2))
+
+elif delta == 0:
+    #cálculo de 1 valor para x
+    x = (-b + math.sqrt(delta)) / (2 * a)
+    print("Para a equação {}x² + {}x + {} = 0, obtivemos o seguinte valor: x = {}".format(a,b,c,x))
+
+else:
+    print("Para a equação {}x² + {}x + {} = 0, não existem valores reais para x".format(a, b, c))
